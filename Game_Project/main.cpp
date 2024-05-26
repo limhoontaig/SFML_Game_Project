deba34d8655df1444a61010d618d0290e837b810
limhoontaig <limhoontaig@gmail.com>
2024-05-26 20:28:36 +0900

    interim

--- Game_Project/main.cpp
+++ Game_Project/main.cpp
@@ -1,5 +1,12 @@
-﻿#include <SFML/Graphics.hpp>
+﻿#define _CRTDBG_MAP_ALLOC
+#include <SFML/Graphics.hpp>
 #include <iostream>
+
+#ifdef _DEBUG
+#define DBG_NEW new ( _NORMAL_BLOCK , __FILE__ , __LINE__ )
+#else
+#define DBG_NEW new
+#endif
 
 int screenWidth = 800;
 int screenHeight = 450;
@@ -8,7 +15,7 @@ float gameSpeed = 0.115f;
 float rectWidth = 20.0f;
 float rectHeight = 20.0f;
 
-void UpdatePosition(float &currentxPosition, float &currentyPosition)
+void UpdatePlayerPosition(float &currentxPosition, float &currentyPosition)
 {
     bool pressedLeft = sf::Keyboard::isKeyPressed(sf::Keyboard::Left);
     bool pressedRight = sf::Keyboard::isKeyPressed(sf::Keyboard::Right);
@@ -40,13 +47,24 @@ void UpdatePosition(float &currentxPosition, float &currentyPosition)
     }
 }
 
+void UpdateEnemyPosition(int numberOfEnemy, float* enemyXPosition, float * enemyYPosition,float playerXPosition, float playerYPosition )
+{
+    for (int i = 0; i < numberOfEnemy; i++)
+    {
+        float enemyToPlayerX = playerXPosition - enemyXPosition[i];
+        float enemyToPlayerY = playerYPosition - enemyYPosition[i];
+    }
+}
+
 int main()
 {
+    _CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
+    
     sf::RenderWindow window(sf::VideoMode(screenWidth, screenHeight), "Game");
     
     sf::RectangleShape player;
-    float enemyXPosition[numberOfEnemy];
-    float enemyYPosition[numberOfEnemy];
+    float* enemyXPosition = new float [numberOfEnemy];
+    float* enemyYPosition = new float [numberOfEnemy];
     const float enemySize = 10.0f;
     const sf::Color enemyColor = { 155, 100, 155 };
     sf::CircleShape enemies[numberOfEnemy];
@@ -63,11 +81,11 @@ int main()
     }
 
 
-    float rectXPosition = 50.0f;
-    float rectYPosition = 50.0f;
+    float playerXPosition = 50.0f;
+    float playerYPosition = 50.0f;
 
     player.setSize(sf::Vector2f(rectWidth, rectHeight));
-    player.setPosition(sf::Vector2f(rectXPosition, rectYPosition));
+    player.setPosition(sf::Vector2f(playerXPosition, playerYPosition));
     player.setFillColor(sf::Color::Red);
     
     while (window.isOpen())
@@ -78,9 +96,9 @@ int main()
             if (event.type == sf::Event::Closed)
                 window.close();
         }
-        UpdatePosition(rectXPosition, rectYPosition);
+        UpdatePlayerPosition(playerXPosition, playerYPosition);
 
-        player.setPosition(sf::Vector2f(rectXPosition, rectYPosition));
+        player.setPosition(sf::Vector2f(playerXPosition, playerYPosition));
 
         window.clear();
         window.draw(player);
@@ -90,6 +108,7 @@ int main()
         }
         window.display();
     }
-
+    delete[] enemyXPosition;
+    delete[] enemyYPosition;    
     return 0;
 }
