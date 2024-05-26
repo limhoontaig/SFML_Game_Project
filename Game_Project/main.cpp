b0dfa7b1e22ce674de3ce857e2c8085f47db1bcd
limhoontaig <limhoontaig@gmail.com>
2024-05-26 11:18:34 +0900

    multi rect

--- Game_Project/main.cpp
+++ Game_Project/main.cpp
@@ -6,27 +6,34 @@ int main()
     int screenHeight = 450;
 
     sf::RenderWindow window(sf::VideoMode(screenWidth, screenHeight), "02_Array_Loop_Condition");
+    sf::RectangleShape rect;
 
     const int numRect = 30;
-    sf::RectangleShape rects[numRect];
+    // sf::RectangleShape rects[numRect];
+    float xPos[numRect];
+    float yPos[numRect];
+    sf::Color color[numRect];
+    float speed[numRect];
 
-    float rectWidth = 20.0f;
-    float rectHeight = 20.0f;
+    float rectWidth[numRect]; // = 20.0f;
+    float rectHeight[numRect];// = 20.0f;
 
     for (int i = 0; i < numRect; i++)
     {
         float randomX = rand() % screenWidth;
         float randomY = rand() % screenHeight;
-
         unsigned int randomR = rand() % 255;
         unsigned int randomG = rand() % 255;
         unsigned int randomB = rand() % 255;
-
-        sf::Color randomColor = sf::Color(randomR, randomG, randomB);
-
-        rects[i] = sf::RectangleShape(sf::Vector2f{ rectWidth, rectHeight });
-        rects[i].setFillColor(randomColor);
-        rects[i].setPosition(sf::Vector2f{ randomX, randomY });
+        xPos[i] = randomX;
+        yPos[i] = randomY;
+        rectWidth[i] = rand() % 40 + 10;
+        rectHeight[i] = rand() % 40 + 10;
+        speed[i] = (rand() % 10) * 0.005f;
+        color[i] = sf::Color(randomR, randomG, randomB);
+        //rects[i] = sf::RectangleShape(sf::Vector2f{ rectWidth, rectHeight });
+        //rects[i].setFillColor(randomColor);
+        //rects[i].setPosition(sf::Vector2f{ randomX, randomY });
     }
 
     while (window.isOpen())
@@ -40,17 +47,23 @@ int main()
 
         for (int i = 0; i < numRect; i++)
         {
-            sf::Vector2f pos = rects[i].getPosition();
-            pos.x += 0.005f;
+            xPos[i] += speed[i];
+            if (xPos[i] == screenWidth)
 
-            rects[i].setPosition(pos);
+            //sf::Vector2f pos = rects[i].getPosition();
+            //pos.x += 0.005f;
+            //rects[i].setPosition(pos);
         }
 
         window.clear();
 
         for (int i = 0; i < numRect; i++)
         {
-            window.draw(rects[i]);
+            rect.setPosition(sf::Vector2f(xPos[i], yPos[i]));
+            rect.setFillColor(color[i]);
+            rect.setSize(sf::Vector2f(rectWidth[i], rectHeight[i]));
+            window.draw(rect);
+            //window.draw(rects[i]);
         }
 
         window.display();
