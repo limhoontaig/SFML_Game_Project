f1eb7830198f4a8c2894c55c4ace586135b57bd7
limhoontaig <limhoontaig@gmail.com>
2024-06-06 09:10:18 +0900

    npc created

--- SFML_KodeJUN/main.cpp
+++ SFML_KodeJUN/main.cpp
@@ -1,14 +1,32 @@
 #include <iostream>
 #include <SFML/Graphics.hpp>
+#include <vector>
 
 int main()
 {
 	sf::RenderWindow window(sf::VideoMode(600, 500), "SFML Works!");
 	window.setFramerateLimit(120);
+
+	// player
 	sf::CircleShape shape(50.0f);
 	shape.setFillColor(sf::Color::Yellow);
 	shape.setPosition(300.0f, 450.0f);
-	sf::RectangleShape myRect(sf::Vector2f{ 50.0f, 15.0f });
+
+
+
+	// target
+	std::vector<sf::RectangleShape> npcs(4);
+
+	int inc = 0;
+	for (auto& _e : npcs)
+	{
+		_e.setSize(sf::Vector2f{ 30.0f, 10.0f });
+		_e.setFillColor(sf::Color::Cyan);
+		_e.setPosition(30.0f+inc, 30.0f+inc);
+		inc += 20.0f;
+	}
+
+	sf::RectangleShape myRect(sf::Vector2f{ 50.0f, 10.0f });
 	myRect.setFillColor(sf::Color::Cyan);
 	myRect.setPosition(sf::Vector2f{ 100.0f, 50.0f });
 		
@@ -36,22 +54,28 @@ int main()
 		}
 
 
+		for (auto& _e:npcs)
+		{ 
+			sf::Vector2f pos = _e.getPosition();
+			if (pos.x > 600.0f)
+			{
+				offset_x = - (float)((rand() % 10) + 1.0f);
+			}
+			else if (pos.x < 30.0f)
+			{
+				offset_x = (float)((rand() % 10) + 1.0f);
+			}
 
-		sf::Vector2f pos = myRect.getPosition();
-		if (pos.x > 600.0f)
-		{
-			offset_x = -1.0f;
+			_e.move(offset_x, 0);
 		}
-		else if (pos.x < 30.0f)
-		{
-			offset_x = 1.0f;
-		}
-
-		myRect.move(offset_x, 0);
 
 		window.clear();
 		window.draw(shape);
-		window.draw(myRect);
+		for (auto& _e : npcs)
+		{
+			window.draw(_e);
+
+		}
 		window.display();
 
 	}
